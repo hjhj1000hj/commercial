118fca5839b3b09c7a529a7ca280c464b651794b
hjhj1000hj <358291459@qq.com>
2017-03-29 21:27:17 +0800

    gencode

---
+++ stonefw.GenCode/SqlServer/SqlServerColumn.cs
@@ -0,0 +1,11 @@
+namespace Stonefw.CodeGenerate.SqlServer
+{
+    public class SqlServerColumn
+    {
+        public string ColumnName { get; set; }
+
+        public int ColumnSize { get; set; }
+
+        public string ColumnType { get; set; }
+    }
+}
---
+++ stonefw.GenCode/SqlServer/SqlServerVersions.cs
@@ -0,0 +1,11 @@
+namespace Stonefw.CodeGenerate.SqlServer
+{
+    internal enum SqlServerVersions
+    {
+        Other,
+        SqlServer2000,
+        SqlServer2005,
+        SqlServer2008,
+        SqlServer2012
+    }
+}
